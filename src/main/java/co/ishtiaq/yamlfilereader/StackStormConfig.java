554c2aa1a8576b58bb2eceac876b9b7b6c74e036
Ishtiaq Maqsood <ishtiaq.maqsood@mastercard.com>
2017-09-12 13:39:44 -0500

    added comments for which annotations work here

--- src/main/java/co/ishtiaq/yamlfilereader/StackStormConfig.java
+++ src/main/java/co/ishtiaq/yamlfilereader/StackStormConfig.java
@@ -2,9 +2,9 @@ package co.ishtiaq.yamlfilereader;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service // @Componnet, @Service, @Configuration all works here
 @ConfigurationProperties(prefix ="stackstorm-columbus" )
 public class StackStormConfig {
 
